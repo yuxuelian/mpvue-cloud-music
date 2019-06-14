d178eb124544ed46676dc15636ca6845b61e9bc4
王开波 <568966289@qq.com>
2019-06-14 18:35:39 +0800

    添加资源文件

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -1,29 +1,50 @@
 <template>
   <div class="component">
     <cu-custom bgColor="bg-gradual-blue" :isBack="true">
-<!--      <block slot="backText">返回</block>-->
+      <!--      <block slot="backText">返回</block>-->
       <block slot="content">导航栏</block>
     </cu-custom>
+
+    <scroll-view scroll-y="" class="content-scroll">
+      <div class="content">
+
+      </div>
+    </scroll-view>
+
+    <div class="cu-bar tabbar bg-white">
+      <div class="action" @click="navChange(0)">
+        <div class='cuIcon-cu-image'>
+          <img v-if="currentIndex===0" src="/static/images/tabbar/basics_cur.png"/>
+          <img v-else src="/static/images/tabbar/basics.png"/>
+        </div>
+        <div :class="currentIndex===0?'text-green':'text-gray'">首页</div>
+      </div>
+      <div class="action " @click="navChange(1)">
+        <div class='cuIcon-cu-image'>
+          <img v-if="currentIndex===1" src="/static/images/tabbar/component_cur.png"/>
+          <img v-else src="/static/images/tabbar/component.png"/>
+        </div>
+        <div :class="currentIndex===1?'text-green':'text-gray'">我的</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
   name: 'index',
-  components: {
-  },
+  components: {},
   props: {},
   data() {
-    return {}
+    return {
+      currentIndex: 0,
+    }
   },
   watch: {},
   computed: {},
   methods: {
-    onClickLeft() {
-      console.log('点击左边')
-    },
-    onClickRight() {
-      console.log('点击右边')
+    navChange(index) {
+      this.currentIndex = index
     },
   },
   created() {
@@ -36,5 +57,21 @@ export default {
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
 
+.component
+  position fixed
+  width 100%
+  height 100%
+  display flex
+  flex-direction column
+
+  .content-scroll
+    background-color #5e00ff
+    width 100%
+    height 0
+    flex 1
+
+    .content
+      width 100%
+      height 4000 rpx
 </style>
 
